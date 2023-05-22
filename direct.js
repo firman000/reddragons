4477892dd895346c2ddb3463f3f2c8fe6b1f9ee6
firman000 <achmadfirman32@gmail.com>
2023-05-23 00:44:03 +0700

    test

---
+++ direct.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://kindnessmarshalping.com/ivpcybwe?key=fe395121b16b549bdc6fd62cd82649fa', '_blank');
+	});
