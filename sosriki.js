bf5c97a5bd45175ab62a7166ec0340065e2f8637
firman000 <achmadfirman32@gmail.com>
2023-07-02 21:47:12 +0700

    Update sosriki.js

--- sosriki.js
+++ sosriki.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : '8b6f4e11989b251731f1c838446890db',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://qualificationsomehow.com/20/cd/8c/20cd8cc2cc058b837d1acd4bddb12cde.js"></scr' + 'ipt>');
