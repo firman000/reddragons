b6f080d6604b9bf6cba013d5ec85e984f16bb86c
firman000 <achmadfirman32@gmail.com>
2023-07-03 09:53:07 +0700

    Update and rename popriki.js to dipopriki.js

---
+++ dipopriki.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://qualificationsomehow.com/pz56v9bb?key=f911c2632c3603cb4904df8ab0771bc4', '_blank');
+	});
--- popriki.js
+++
@@ -1,1 +0,0 @@
-src='//qualificationsomehow.com/d5/89/11/d58911df321c91eed085fe64c382707b.js
